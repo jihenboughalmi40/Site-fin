bc6bffc13c96209f7e2fec78dd2d4575c82faad8
snowden96 <moneem.benbelgasem@esen.tn>
2021-11-24 20:45:23 +0100

    firstt

--- wp-content/themes/Tunisien-Sport/loop.php
+++ wp-content/themes/Tunisien-Sport/loop.php
@@ -1,7 +1,7 @@
 <?php if(have_posts()) : ?>
     <?php while(have_posts()) : the_post(); ?>
         <div class="blog-post">
-            <?php (is_singular()) ? the_content() : the_excerpt(); ?>
+          
         </div>
     <?php endwhile; ?>
     <div id="pagination">
